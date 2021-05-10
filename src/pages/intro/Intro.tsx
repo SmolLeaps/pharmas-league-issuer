fd015cc9fe5717a337417f6a3342fa90804c8ec6
Callista Chang <changcallista@gmail.com>
2021-05-10 17:31:18 +0800

    Revamped homepage

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'pages/intro/Intro.scss'
-import EntireFlow from 'assets/images/icons/user-flow-placeholder.png';
+import EntireFlow from 'assets/images/icons/user-flow.png';
+import NewsArticles from 'assets/images/icons/news-pharma-fakes.png';
 import {routes} from 'constants/routes';
 
 /**
@@ -11,33 +12,36 @@ const IntroPage = () => {
     <div className='intro page-form page-form--large'>
       <div className='intro__heading-block'>
         <h1 className='intro__heading'>
-          Pharmas League
+          Pharma's League
         </h1>
-        <h5 className='intro__subheading'>Greater Southeast Asia's Trusted Drug Marketplace.</h5>
+        <h5 className='intro__subheading'>Greater Southeast Asia's Trusted Drug Marketplace</h5>
       </div>
       <div className='intro__text-block'>
-        <h4>Reseller - Getting your First Verifiable Credential</h4>
-        <p></p>
+        <h4>Rise in Counterfeit Drugs Sold Online</h4>
+        <p>The ongoing COVID-19 pandemic and the fast-paced world of e-commerce has taken the acceleration of selling pharmaceuticals online to the next level.</p>
+        <p>Though 60% of pharmaceutical brands have seen an increase of revenue from their online business, they have suffered an increase of 67% in online counterfeits as a result as well. WHO estimates that counterfeit medicines worth 89 billion are traded annually.</p> 
+        <p>Multiple news articles have been published about the increasingly prevalent issue:</p>
+        <img className='articles-size' src={NewsArticles} alt='entire-flow' />
       </div>
-      <div className='intro__example'>
+      <div className='intro__text-block'>
+        <h4>Issuer - Holder - Verifier Flow)</h4>
         <img className='flow-size' src={EntireFlow} alt='entire-flow'/>
       </div>
-      <div className='intro__roles-description'>
+      {/* <div className='intro__roles-description'>
         <div className='intro__roles-description-role'>
           <h3>Issuer</h3>
-          <p>TODO</p>
+          <p>Pharma A is able to issue digitalized verfiable credentials.</p>
         </div>
         <div className='intro__roles-description-role'>
           <h3>Holder</h3>
-          <p>TODO</p>
+          <p></p>
         </div>
         <div className='intro__roles-description-role'>
           <h3>Verifier</h3>
           <p>TODO</p>
         </div>
       </div>
-
-      <p>Ready to list your drug on Pharma's League? Register <a href={routes.APPLICANT_LOGIN} rel='noreferrer'>here</a></p>
+      <p>Ready to list your drug on Pharma's League? Register <a href={routes.APPLICANT_LOGIN} rel='noreferrer'>here</a></p> */}
     </div>
   )
 }
